0d49c7be3d907b12b394fb4bef7d1eaafd2131ef
guroy <guroy@insa-rennes.fr>
2015-09-25 23:36:28 -0400

    pending

--- Lab 2 - Polygon Fill/Rasterizer.cpp
+++ Lab 2 - Polygon Fill/Rasterizer.cpp
@@ -9,6 +9,7 @@
 
 #include "Rasterizer.h"
 #include "simpleCanvas.h"
+#include <float.h>
 
 using namespace std;
 
@@ -43,4 +44,71 @@ Rasterizer::Rasterizer (int n) : n_scanlines (n)
 void Rasterizer::drawPolygon(int n, int x[], int y[], simpleCanvas &C)
 {
     // YOUR IMPLEMENTATION GOES HERE
+	int nbEdges = sizeof(x);
+
+
+	/*
+		Initializing All of the Edges
+	*/
+	
+	// we define a Bucket structure to store, for each edge, the following values
+	typedef struct {
+		int yMin, // The minimum y value of the two vertices
+			yMax, // The maximum y value of the two vertices
+			xVal; // The x value associated with the minimum y value
+		float inv_m; // 1/m, the inverse of the slope
+	} Bucket ;
+	
+	// we allocate an Array that will contain all the Buckets of the polygon
+	Bucket *globalEdgeTable = new Bucket[nbEdges - 1];
+
+	// we count the number of horizontal lines we forget, as we don't need to store them
+	int nbHorizontalLines = 0;
+
+	for (int i = 0; i < nbEdges - 1; i++) {
+		Bucket b;
+		// let's initialize the current bucket
+		b.yMin = min(y[i], y[i + 1]);
+		b.yMax = max(y[i], y[i + 1]);
+		b.xVal = b.yMin == y[i] ? x[i] : x[i + 1];
+		b.inv_m = b.yMax - b.yMin != 0 ?
+			abs(x[i] - x[i + 1]) / (b.yMax - b.yMin) : FLT_MAX;
+		// we don't need to store horizontal lines. As we have the 
+		if (b.inv_m != FLT_MAX)
+			globalEdgeTable[i] = b;
+		else
+			nbHorizontalLines++;
+	}
+
+	// here is the number of buckets we truly keep after having removed the horizontal edges
+	nbEdges -= nbHorizontalLines;
+	
+
+	/*
+		Initializing the Global Edge Table
+	*/
+
+	Bucket b;
+
+	// we need to sort the buckets...
+	for (int i = 0; i < nbEdges - 1; i++) {
+		for (int j = i; j < nbEdges - 1; j++) {
+			// sort by yMin...
+			if (globalEdgeTable[j].yMin <= globalEdgeTable[i].yMin) {
+				// or by xVal if yMin is the same
+				if (globalEdgeTable[j].yMin < globalEdgeTable[i].yMin || globalEdgeTable[j].xVal < globalEdgeTable[i].xVal) {
+					b = globalEdgeTable[j];
+					globalEdgeTable[j] = globalEdgeTable[i];
+					globalEdgeTable[i] = b;
+				}
+			}
+		}
+	}
+
+
+	/*
+		Initializing parity
+	*/
+
+	free(globalEdgeTable);
 }
