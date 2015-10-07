2095a4356123ee94a8230d81038941178eade5db
guroy <guroy@insa-rennes.fr>
2015-10-06 23:03:12 -0400

    wip

--- Lab 3 - Clipping/clipper.cpp
+++ Lab 3 - Clipping/clipper.cpp
@@ -4,7 +4,7 @@
 //  Created by Joe Geigel on 11/30/11.
 //  Copyright 2011 Rochester Institute of Technology. All rights reserved.
 //  
-//  Contributor:  YOUR_NAME_HERE
+//  Contributor:  Guillaume Roy
 //
 
 #include "clipper.h"
@@ -51,8 +51,93 @@ int clipper::clipPolygon(int in, const float inx[], const float iny[],
                 float outx[], float outy[],
 		float x0, float y0, float x1, float y1)
 {
-    // YOUR IMPLEMENTATION GOES HERE
+	// YOUR IMPLEMENTATION GOES HERE
+	int out = 0;// , out2 = 0, out3 = 0, out4 = 0;
 
-    return 0;  // should return number of vertices in clipped poly.
+	//float *outx2, *outy2, *outx3, *outy3, *outx4, *outy4;
 
+	SHPC(inx, iny, outx, outy, in, out, x1, y1, x0, y1); // left
+	//outx2 = new float[out]; outy2 = new float[out];
+	//SHPC(outx, outy, outx2, outy2, out, out2, x0, y0, x1, y0); // bottom
+	//outx3 = new float[out2]; outy3 = new float[out2];
+	//SHPC(outx2, outy2, outx3, outy3, out2, out3, x1, y0, x1, y1); // right
+	//outx4 = new float[out3]; outy4 = new float[out3]; 
+	//SHPC(outx3, outy3, outx4, outy4, out3, out4, x1, y1, x0, y1); // top
+
+	/*outx = outx4;
+	outy = outy4;
+
+	delete [] outx2, outy2, outx3, outy3;*/
+	return out;
+}
+
+void clipper::SHPC(const float inx[], const float iny[],
+	float outx[], float outy[], int in, int &out,
+	float x0, float y0, float x1, float y1) {
+
+	float px = inx[in - 1], py = iny[in - 1]; // last vertex is the initial “predecessor”
+
+	float _x = px, _y = py;
+
+	for (int j = 0; j < in; j++) {
+		if (inside(inx[j], iny[j], x0, y0, x1, y1)) { // Cases 1 & 4
+			if (inside(px, py, x0, y0, x1, y1)) { // Case 1
+				output(inx[j], iny[j], out, outx, outy);
+			}
+			else { // Case 4
+				intersect(px, py, inx[j], iny[j], x0, y0, x1, y1, _x, _y);
+				output(_x, _y, out, outx, outy);
+				output(inx[j], iny[j], out, outx, outy);
+			}
+		}
+		else { // Cases 2 & 3
+			if (inside(px, py, x0, y0, x1, y1)) { // Case 2
+				intersect(px, py, inx[j], iny[j], x0, y0, x1, y1, _x, _y);
+				output(_x, _y, out, outx, outy);
+			} // Case 3 has no output 
+		}
+
+		px = inx[j];
+		py = iny[j];
+
+	} // for
+}
+
+
+// is point inside boundary?
+bool clipper::inside(float _x, float _y, float x0, float y0, float x1, float y1) {
+	if (y0 == y1) { // horizontal edge
+		if (x0 < x1) return _y >= y0;
+		if (x0 > x1) return _y <= y0;
+	} else { // vertical edge
+		if (y1 > y0) return _x <= x0;
+		if (y0 > y1) return _x >= x0;
+	}
+	return false;
+}
+
+
+// put point into vector, update length
+void clipper::output(float _x, float _y, int &out, float outx[], float outy[]) {
+	outx[out] = _x;
+	outy[out++] = _y;
+}
+
+
+// compute intersection point, put point into newpoint parameter
+void clipper::intersect(float sx, float sy, float ex, float ey, float x0, float y0, float x1, float y1, float &_x, float &_y) {
+	float m, B;
+
+	if (x0 == x1) { // if it's a vertical edge
+		m = (ey - sy) / (ex - sx);
+		B = ey - m * ex;
+		_x = x0;
+		_y = m * _x + B;
+	}
+	else { // if it's a horizontal edge
+		m = (ex - sx) / (ey - sy);
+		B = ey - m * ex;
+		_y = y0;
+		_x = (_y - B) / m;
+	}
 }
--- Lab 3 - Clipping/clipper.h
+++ Lab 3 - Clipping/clipper.h
@@ -4,7 +4,7 @@
 //  Created by Joe Geigel on 11/30/11.
 //  Copyright 2011 Rochester Institute of Technology. All rights reserved.
 //  
-//  Contributor:  YOUR_NAME_HERE
+//  Contributor:  Guillaume Roy
 //
 
 #ifndef _CLIPPER_H_
@@ -54,6 +54,82 @@ public:
                     float outx[], float outy[],
 		    float x0, float y0, float x1, float y1);
 
+	///
+	// inside
+	//
+	// Tell if a given point is inside the boundaries
+	//
+	// @param _x the x coordinate of the point we test
+	// @param _y the y coordinate of the point we test
+	// @param x0 - x coord of lower left of clipping rectangle.
+	// @param y0 - y coord of lower left of clipping rectangle.
+	// @param x1 - x coord of upper right of clipping rectangle.
+	// @param y1 - y coord of upper right of clipping rectangle.
+	//
+	// @return true if the point is inside the boundaries
+	//
+	///
+	bool inside(float _x, float _y, float x0, float y0, float x1, float y1);
+
+	///
+	// output
+	//
+	// add a given point to the output list of vertices and increment the
+	// number of points consequently
+	//
+	// @param _x the x coordinate of the point we want to add
+	// @param _y the y coordinate of the point we want to add
+	// @param out the number of vertices in outx and outy tables
+	// @param outx - x coords of vertices of polygon resulting after clipping.
+	// @param outy - y coords of vertices of polygon resulting after clipping.
+	//
+	///
+	void output(float _x, float _y, int &out, float outx[], float outy[]);
+
+	///
+	// interesc
+	//
+	// Compute intersection of a line with a boundary
+	// Will modify the values of _x and _y so that they
+	// contain the coordinate of the intersection
+	// we evaluate the intersection between SE and AB,
+	// where AB is one of the 4 edges of the window
+	//
+	// @param sx the x coordinate of the start point of the line
+	// @param sy the y coordinate of the start point of the line
+	// @param ex the x coordinate of the end point of the line
+	// @param ey the y coordinate of the end point of the line
+	// @param x0 - x coord of lower left of clipping rectangle.
+	// @param y0 - y coord of lower left of clipping rectangle.
+	// @param x1 - x coord of upper right of clipping rectangle.
+	// @param y1 - y coord of upper right of clipping rectangle.
+	// @param _x the x coordinate of the intersection
+	//
+	///
+	void intersect(float sx, float sy, float ex, float ey, float x0, float y0, float x1, float y1, float &_x, float &_y);
+
+	///
+	// SHPC
+	//
+	// Apply SHPC algorithm for one edge
+	//
+	// @param inx - x coords of vertices of polygon to be clipped.
+	// @param iny - y coords of vertices of polygon to be clipped.
+	// @param outx - x coords of vertices of polygon resulting after clipping.
+	// @param outy - y coords of vertices of polygon resulting after clipping.
+	// @param in the number of vertices in the polygon to be clipped
+	// @param out the number of vertices of the output polygon
+	// @param x0 - x coord of lower left of clipping rectangle.
+	// @param y0 - y coord of lower left of clipping rectangle.
+	// @param x1 - x coord of upper right of clipping rectangle.
+	// @param y1 - y coord of upper right of clipping rectangle.
+	//
+	// @return number of vertices in the polygon resulting after clipping
+	//
+	///
+	void SHPC(const float inx[], const float iny[],
+		float outx[], float outy[], int in, int &out,
+		float x0, float y0, float x1, float y1);
 };
 
 #endif
