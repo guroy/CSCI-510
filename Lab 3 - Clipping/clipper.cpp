f642d9ff598a2c2717853710d0e146dbc9a67a21
guroy <guroy@insa-rennes.fr>
2015-10-06 23:28:51 -0400

    works!

--- Lab 3 - Clipping/clipper.cpp
+++ Lab 3 - Clipping/clipper.cpp
@@ -52,22 +52,31 @@ int clipper::clipPolygon(int in, const float inx[], const float iny[],
 		float x0, float y0, float x1, float y1)
 {
 	// YOUR IMPLEMENTATION GOES HERE
-	int out = 0;// , out2 = 0, out3 = 0, out4 = 0;
+	int out = 0, out2 = 0, out3 = 0, out4 = 0;
 
-	//float *outx2, *outy2, *outx3, *outy3, *outx4, *outy4;
+	float *outx2, *outy2, *outx3, *outy3, *outx4, *outy4;
 
-	SHPC(inx, iny, outx, outy, in, out, x1, y1, x0, y1); // left
-	//outx2 = new float[out]; outy2 = new float[out];
-	//SHPC(outx, outy, outx2, outy2, out, out2, x0, y0, x1, y0); // bottom
-	//outx3 = new float[out2]; outy3 = new float[out2];
-	//SHPC(outx2, outy2, outx3, outy3, out2, out3, x1, y0, x1, y1); // right
-	//outx4 = new float[out3]; outy4 = new float[out3]; 
-	//SHPC(outx3, outy3, outx4, outy4, out3, out4, x1, y1, x0, y1); // top
+	// We clip entire figure by clipping on the 4 edges of the clipping window
+	// each step will be stored in *out pointers.
 
-	/*outx = outx4;
-	outy = outy4;
+	SHPC(inx, iny, outx, outy, in, out, x0, y1, x0, y0); // left
 
-	delete [] outx2, outy2, outx3, outy3;*/
+	outx2 = new float[out]; outy2 = new float[out];
+	SHPC(outx, outy, outx2, outy2, out, out2, x0, y0, x1, y0); // bottom
+
+	outx3 = new float[out2]; outy3 = new float[out2];
+	SHPC(outx2, outy2, outx3, outy3, out2, out3, x1, y0, x1, y1); // right
+
+	outx4 = new float[out3]; outy4 = new float[out3]; 
+	SHPC(outx3, outy3, outx4, outy4, out3, out4, x1, y1, x0, y1); // top
+
+	// we want outx and outy to store the final clipped polygons, which
+	// means we need to get content of outx4 and outy4
+	for (int i = 0; i < out; i++) { outx[i] = outx4[i];	outy[i] = outy4[i]; }
+	// same principle for out value
+	out = out4;
+
+	delete[] outx2, outy2, outx3, outy3, outx4, outy4;
 	return out;
 }
 
@@ -126,18 +135,12 @@ void clipper::output(float _x, float _y, int &out, float outx[], float outy[]) {
 
 // compute intersection point, put point into newpoint parameter
 void clipper::intersect(float sx, float sy, float ex, float ey, float x0, float y0, float x1, float y1, float &_x, float &_y) {
-	float m, B;
-
 	if (x0 == x1) { // if it's a vertical edge
-		m = (ey - sy) / (ex - sx);
-		B = ey - m * ex;
 		_x = x0;
-		_y = m * _x + B;
+		_y = sy + (x0 - sx) * (ey - sy) / (ex - sx);
 	}
 	else { // if it's a horizontal edge
-		m = (ex - sx) / (ey - sy);
-		B = ey - m * ex;
 		_y = y0;
-		_x = (_y - B) / m;
+		_x = sx + (y0 - sy) * (ex - sx) / (ey - sy);
 	}
 }
