367aa56deda0178c294355a73198188b34b8c578
guroy <guroy@insa-rennes.fr>
2015-11-30 10:50:57 -0500

    raz

--- Lab 6 - Shading/lightingParams.cpp
+++ Lab 6 - Shading/lightingParams.cpp
@@ -9,6 +9,21 @@
 #include "lightingParams.h"
 
 // Add any global variables you need here.
+// Material properties of the teapot and torus
+float ambient_color[] = { 0.5f, 0.1f, 0.9f, 1.0f },
+	ambient_reflection_coefficient = 0.5f,
+	diffuse_color[] = { 0.89f, 0.0f, 0.0f, 1.0f },
+	diffuse_reflection_coefficient = 0.7f,
+	specular_color[] = { 1.0f, 1.0f, 1.0f, 1.0f },
+	specular_exponent = 10.0f,
+	specular_reflection_coefficient = 1.0f;
+
+// Properties of the light source
+float lightSourceColor[] = { 1.0f, 1.0f, 0.0f, 1.0f },
+	lightSourcePosition[] = { 0.0f, 5.0f, 2.0f, 1.0f };
+
+// Properties of the ambient light
+float ambientLightColor[] = { 0.5f, 0.5f, 0.5f, 1.0 };
 
 /**
  * This function sets up the lighting, material, and shading parameters
@@ -24,4 +39,32 @@
 void setUpPhong( GLuint program )
 {
     // Add your code here.
+	glUniform4fv(
+		glGetUniformLocation(program, "lightSourceColor"),
+		1, lightSourceColor);
+	glUniform4fv(
+		glGetUniformLocation(program, "lightSourcePosition"),
+		1, lightSourcePosition);
+	glUniform4fv(
+		glGetUniformLocation(program, "ambient_color"),
+		1, ambient_color);
+	glUniform4fv(
+		glGetUniformLocation(program, "diffuse_color"),
+		1, diffuse_color);
+	glUniform4fv(
+		glGetUniformLocation(program, "specular_color"),
+		1, specular_color);
+
+	/*glUniform1f(
+		glGetUniformLocation(program, "ambient_reflection_coefficient"),
+		ambient_reflection_coefficient);
+	glUniform1f(
+		glGetUniformLocation(program, "diffuse_reflection_coefficient"),
+		diffuse_reflection_coefficient);
+	glUniform1f(
+		glGetUniformLocation(program, "specular_reflection_coefficient"),
+		specular_reflection_coefficient);*/
+	glUniform1f(
+		glGetUniformLocation(program, "specular_exponent"),
+		specular_exponent);
 }
